931afc66a79e8f33fa5af425a59757ec032aef79
ghanteyyy <ghanteyyy@gmail.com>
2026-04-16 19:36:17 +0545

    Added tables

---
+++ models/user.py
@@ -0,0 +1,23 @@
+import uuid
+import enum
+from datetime import datetime, timezone
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, String, Enum, Date, DateTime
+from db.base import Base
+
+
+class GenderEnum(str, enum.Enum):
+    male = "male"
+    female = "female"
+    other = "other"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    gender = Column(Enum(GenderEnum), nullable=False)
+    dob = Column(Date, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
