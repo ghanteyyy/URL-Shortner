6617ae56a491698df3e25790bc9a01a3e892eca4
ghanteyyy <ghanteyyy@gmail.com>
2026-04-18 20:01:53 +0545

    Fixed imports

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from db.session import engine
 from db.base import Base
 from auth import routes as auth_routes
-from jwt import routes as jwt_routes
 
 Base.metadata.create_all(bind=engine)
 
