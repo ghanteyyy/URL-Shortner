ac441bbd9e447b8024379ba8be38df17d6af6ffa
ghanteyyy <ghanteyyy@gmail.com>
2026-04-18 20:39:44 +0545

    Updated title and logo for Swagger UI

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 from db.session import engine
 from db.base import Base
 from auth import routes as auth_routes
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="URL Shortner API Docs", description="FastAPI URL shortener with JWT auth and PostgreSQL", docs_url=None)
+app.mount('/static', StaticFiles(directory="static"), name="static")
+
 app.include_router(auth_routes.router, prefix="/api/auth", tags=['auth'])
+
+@app.get("/docs", include_in_schema=False)
+async def custom_docs():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title="URL Shortner Docs",
+        swagger_favicon_url="/static/icon.png",
+    )
